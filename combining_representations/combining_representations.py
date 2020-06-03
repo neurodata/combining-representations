fd06cba670e7a9e922f92231843d2e1561e807b1
hhelm <hhelm2@jhu.edu>
2020-06-03 11:58:23 -0400

    initial PuLP implementation

--- combining_representations/combining_representations.py
+++ combining_representations/combining_representations.py
@@ -1,20 +1,22 @@
-import gurobipy as gp
-from gurobipy import GRB
-
+import pulp
 import numpy as np
 
 
-def combine_representations(dist_matrix, voi_index, S_indices, model_name=None, return_new_dists=True):
+def combine_representations(dist_matrix, voi_index, S_indices, return_new_dists=True):
     """
     A function to find the weights of optimal linear combination of representations.
     
     Input
     dist_matrix - np.array (shape=(n, J))
         Array containing the distances between the vertex of interest and the other n - 1
-        vertices. It is assumed that the vertex of interest is indexed by 0.
+        vertices.
+    voi_index - int
+        Index of vertex of interest.
     S_indices - array-like
-        Array-like containing the indices of the vertices that should be at the top of the
+        Indices of the vertices that should be at the top of the
         nomination list for the vertex of interest.
+    return_new_dists - bool
+        If true, returns both the weights and the corresponding distance matrix
         
     Return
     weights - np.array (length=J)
@@ -30,34 +32,50 @@ def combine_representations(dist_matrix, voi_index, S_indices, model_name=None, 
     Q = len(Q_indices)
     
     M = np.sum(abs(dist_matrix))
-    
-    if model_name is not None:
-        m = gp.Model(name='%s'%(model_name))
-    else:
-        m= gp.Model()
-        
-    m.setParam('OutputFlag', 0)
 
-    ind = m.addVars(Q, vtype=GRB.BINARY, name='ind')
-    m.setObjective(gp.quicksum(ind), GRB.MINIMIZE)
+    model=pulp.LpProblem(sense=pulp.LpMinimize)
+    ind = pulp.LpVariable.dicts("indicators for elements not in S", 
+                                (q for q in Q_indices),
+                                cat='Integer',
+                               upBound=1,
+                               lowBound=0)
 
-    w = m.addVars(J, lb=0, ub=1, vtype=GRB.CONTINUOUS, name='w')
-    m.addConstr(w.sum() == 1)
+    weights = pulp.LpVariable.dicts("weights for representations",
+                                   (j for j in range(J)),
+                                   cat='Continuous',
+                                   upBound=1,
+                                   lowBound=0
+                                   )
 
-    # There's likely a more pythonic way to set up these constraints (in particular using m.addConstrs(..))
+    model += (
+        pulp.lpSum(
+            [ind[(q)] for q in Q_indices]
+        )
+    )
+
+    model += (
+        pulp.lpSum(
+            [weights[(j)] for j in range(J)]
+        ) == 1
+    )
+
     for s in S_indices:
-        temp_s = gp.tupledict([((i), dist_matrix[s, i]) for i in range(J)])
-        for i, q in enumerate(Q_indices):
-            temp_q = gp.tupledict([((i), dist_matrix[q, i]) for i in range(J)])
-            m.addConstr(w.prod(temp_s) <= w.prod(temp_q) + ind[i]*M)
-        
-    m.optimize()
-    
-    alpha_hat = np.array([i.X for i in list(w.values())])
-    
-    if model_name:
-        m.write('%s.ip'%(model_name))
-        
+        for q in Q_indices:
+            model += (
+                pulp.lpSum(
+                    [weights[(j)] * dist_matrix[s, j] for j in range(J)]
+                )
+                <=
+                pulp.lpSum(
+                    [weights[(j)] * dist_matrix[q, j] for j in range(J)]
+                ) 
+                + 
+                pulp.lpSum(ind[(q)] * M)
+            )
+
+    model.solve()
+    alpha_hat = np.array([w.varValue for w in weights.values()])
+
     if return_new_dists:
         return alpha_hat, np.average(dist_matrix, axis=1, weights=alpha_hat)
     
